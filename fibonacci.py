887561778c6d3cbd23d9b244f3f2ec6505e878c2
fnocci <fnocci@users.noreply.github.com>
2019-07-14 07:50:54 -0400

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+######################################
+# fibonacci series
+######################################
+
+# recursive 
+def fib(n):
+    if n < 0: return None
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return fib(n-1)+fib(n-2)
+
+# memoized recursive
+memo = {0:0, 1:1}
+def mfib(n):
+    if n < 0: return None
+    if n in memo: return memo[n] 
+    else: return mfib(n-1)+mfib(n-2)
